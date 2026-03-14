d64a77bed5706f2c32bf380e088da60dc33c164e
Axmedov Sulaymon <playzone937@gmail.com>
2026-03-14 16:09:18 +0500

    bot yasaldi
    bu botda tushuntirildi

---
+++ bots.py
@@ -0,0 +1,13 @@
+import asyncio 
+from aiogram import Bot , Dispatcher ,types   
+from aiogram.filters import Command 
+Token = ''
+bot = Bot(token = Token) #token olish
+db = Dispatcher() #dispechirdan obyekt olsih
+@db.message(Command("start")) # userdan kelgan buyruqni filtirlash
+async def Start_hadler(message : types.Message):
+    await message.answer("Botga kirishdan maqsadingiz nima ?") #bu userga malumot chiqarish
+async def main():
+    await db.start_polling(bot) # botni ishga tushuruvchi Funksiya
+asyncio.run(main())
+
