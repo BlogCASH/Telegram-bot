36ad4cb09f1c1f30478210d195825cf61f3bf203
Axmedov Sulaymon <playzone937@gmail.com>
2026-03-17 16:16:14 +0500

    bu botda butten bor
    botga tugma kirittik

--- bots.py
+++ bots.py
@@ -1,23 +1,72 @@
 import asyncio 
 from aiogram import Bot , Dispatcher ,types   
 from aiogram.filters import Command 
-Token = ''
-bot = Bot(token = Token) #token olish
-db = Dispatcher() #dispechirdan obyekt olsih
+from aiogram.types import ReplyKeyboardMarkup , KeyboardButton
+
+
+TOKEN = ''
+
+bot = Bot(token = TOKEN) #token olish
+
+db = Dispatcher() #dispechirdan obyekt olish
+
+kb = ReplyKeyboardMarkup(
+    keyboard = [[KeyboardButton(text="Kurslar")]],
+
+    resize_keyboard=True
+)
+
+
+
 @db.message(Command("start")) # userdan kelgan buyruqni filtirlash
 async def Start_hadler(message : types.Message):
-    await message.answer("Botga kirishdan maqsadingiz nima ?") #bu userga malumot chiqarish
+    await message.answer("Qaysi kurslarimizda o'qimoqchisiz",  reply_markup=kb) #bu userga malumot chiqarish
 
 @db.message(Command("help")) # userdan kelgan buyruqni filtirlash
 async def Start_had(message : types.Message):
-    await message.answer("Botimizning qayeriga tushunmayabsiz ?") #bu userga malumot chiqarish
+    await message.answer("Nima savol qiynayabti ?") #bu userga malumot chiqarish
 
 @db.message(Command("call")) # userdan kelgan buyruqni filtirlash
 async def Start_had(message : types.Message):
-    await message.answer("biz bilan bog'lanish +998 ** *** ** **") #bu userga malumot chiqarish
+    await message.answer("Biz bilan bog'lanish +998 ** *** ** **") #bu userga malumot chiqarish
 
-
+@db.message()
+async def msg(message: types.Message):
+    if message.text == "Salom":
+        await message.answer("Bizning kurslarimiz . IT , Ingilis tili va boshqalar")
+    
 async def main():
     await db.start_polling(bot) # botni ishga tushuruvchi Funksiya
 asyncio.run(main())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
